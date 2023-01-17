5fbabc1a92c4187349bcdf1f5d8dc839fbd700e3
M. Atakan Gurkan <ato.gurkan@gmail.com>
2023-01-17 14:11:25 +0300

    drawing circles

--- learning-JS/main.js
+++ learning-JS/main.js
@@ -1,1 +1,11 @@
-console.log('main.js loaded')
+$(document).ready(function() {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+    
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+
+    paper.view.draw();
+    //console.log('main.js loaded');
+});
